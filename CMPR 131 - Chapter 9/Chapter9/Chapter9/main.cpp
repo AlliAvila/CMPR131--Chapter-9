db5cd1f47597386d00df6d0042792ec96d5aa455
Davidsworld <davidderramadero3@gmail.com>
2025-11-06 20:56:26 -0800

    Refactor main.cpp for clarity and structure

--- CMPR 131 - Chapter 9/Chapter9/Chapter9/main.cpp
+++ CMPR 131 - Chapter 9/Chapter9/Chapter9/main.cpp
@@ -78,9 +78,9 @@ void printStars(int n)
 // Post-Condtion: Prints a pattern of * recursively separated by spaces
 void pattern(int level, int spaces)
 {
-	if (level > 1) return; // Base case: stop after printing the bottom side
+	if (level > 1) return; // Base case: stop after level 3
 
-	// Line 1: 1 star *
+	// Line 1: 1 star
 	printSpaces(spaces);
 	printStars(1);
 	cout << "\n";
@@ -116,7 +116,7 @@ void pattern(int level, int spaces)
 	cout << "\n";
 
 	// Line 8: 8 stars
-	if (level == 1) // Other base case to not print the 8 * stars
+	if (level == 1)
 		return;
 
 	printSpaces(spaces);
@@ -150,11 +150,18 @@ void guessNumber(int low, int high, int count)
 		cout << "\n\tNumber of guesses: " << count;
 		return;
 	}
-
 	int guess = (low + high) / 2;
+
+	
 	if (toupper(inputChar("\n\tIs your number " + to_string(guess) + "? (Y-yes or N-no): ", static_cast<string>("yn"))) == 'Y')
 	{
 		cout << "\n\tNumber of guesses: " << count << " Yay!";
+		return;
+	}
+	else if (guess == 1)
+	{
+		cout << "\n\tYour number must be " << low << "!";
+		cout << "\n\tNumber of guesses: " << count;
 		return;
 	}
 	if (toupper(inputChar("\tIs your number larger than " + to_string(guess) + "? (Y-yes or N-no): ", static_cast<string>("yn"))) == 'Y')
@@ -165,7 +172,6 @@ void guessNumber(int low, int high, int count)
 	{
 		high = guess - 1;
 	}
-
 	count++;
 
 	guessNumber(low, high, count);
@@ -197,5 +203,3 @@ void option4()
 
 	bool solved = game.solveNQueens(firstColumnPosition);
 }
-
-
